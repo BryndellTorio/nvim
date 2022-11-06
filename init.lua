942cb4771ff0682c3aade2c169f6422064048cdf
Del <bryndelltorio@outlook.com>
2022-11-06 20:40:43 +0800

    Initial nvim setup, Just finished integrating text folding next will be optimizing LSP autocompletion.

---
+++ init.lua
@@ -0,0 +1,10 @@
+-- reference setup: https://www.youtube.com/watch?v=_-b2G66FL5U
+
+require('packer-config')
+require('settings')
+require('mappings')
+require('colors')
+require('nvim-tree-config')
+require('lsp-config.lang-server')
+require('lsp-config.nvim-cmp')
+require('nvim-treesit-conf')
---
+++ lua/mappings/init.lua
@@ -0,0 +1,14 @@
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+vim.g.mapleader = ' '
+
+map('n', '<leader>t', ':NvimTreeToggle<CR>', opts)
+map('n', '<leader>nh', ':noh<CR>', opts)
+
+map('n', '<C-J>', '<C-W><C-J>', opts)
+map('n', '<C-K>', '<C-W><C-K>', opts)
+map('n', '<C-L>', '<C-W><C-L>', opts)
+map('n', '<C-H>', '<C-W><C-H>', opts)
+
+map('n', '<C-]>', ':foldclose', opts)
